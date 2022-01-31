9b229b8edb51fdcb705f142f4604cdbc51df0c9c
Tiago Silva <tbrancosilva@hotmail.com>
2022-01-31 13:38:55 +0000

    ➕ Menus

--- src/Main.c
+++ src/Main.c
@@ -5,11 +5,58 @@
 #include "Main.h"
 #include <stdio.h>
 #include "Vehicles.h"
+#include "./helpers/Menus.h"
 
 int main() {
-    printf("Entry Point\n\n");
+    int menuChoice;
 
-//    bootstrapVehicles();
-    readVehicles();
-//    printVehicles();
+    printf("*** Welcome to the freight company software *** \n\n");
+
+    do {
+        printMainMenu();
+
+        printf("Select an option from the menu: ");
+        scanf("%i", &menuChoice);
+
+        switch (menuChoice) {
+            case 1:
+                printFreightsMenu();
+
+                break;
+
+            case 2:
+                printTripsMenu();
+
+                break;
+
+            case 3:
+                printClientsMenu();
+
+                break;
+
+            case 4:
+                VehiclesMenu();
+
+                break;
+
+            case 5:
+                printClientsMenu();
+
+                break;
+
+            case 6:
+//                writeAllFiles()
+
+                break;
+
+            default:
+                break;
+        }
+    } while (menuChoice != 0);
 }
+
+
+//int main() {
+//    bootstrapVehicles();
+//    printVehicles();
+//}
