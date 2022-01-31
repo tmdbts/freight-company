ddab4d38803362f7c34be3215c6181f2aaf02fdc
Tiago Silva <tbrancosilva@hotmail.com>
2022-01-31 13:34:10 +0000

    ✨ Restructured clients

--- src/Clients.c
+++ src/Clients.c
@@ -4,11 +4,3 @@
 
 #include "Clients.h"
 
-typedef struct client {
-    int id;
-    char name[46];
-    int nif;
-    char address[46];
-    char city[46];
-    int zipCode;
-} CLIENT;
--- src/Clients.h
+++ src/Clients.h
@@ -5,6 +5,23 @@
 #ifndef FREIGHT_COMPANY_CLIENTS_H
 #define FREIGHT_COMPANY_CLIENTS_H
 
-typedef struct client CLIENT;
+typedef struct client {
+    int id;
+    char name[46];
+    int nif;
+    char address[46];
+    char city[46];
+    int zipCode;
+} CLIENT;
+
+CLIENT clients[100];
+
+void createClient();
+
+void readClients();
+
+void updateClient();
+
+void deleteClient();
 
 #endif //FREIGHT_COMPANY_CLIENTS_H
