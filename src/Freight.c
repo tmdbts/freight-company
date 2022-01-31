5d40347bba98ba55d473657cf062943de1f77139
Tiago Silva <tbrancosilva@hotmail.com>
2022-01-31 13:34:39 +0000

    ✨ Restructured freights

--- src/Freight.c
+++ src/Freight.c
@@ -4,21 +4,3 @@
 
 #include "Freight.h"
 
-typedef struct freight {
-    int id;
-    int clientId;
-//    char designation;
-    char priority[36];
-    char preferablePickUpDate[10];
-    char pickUpDate[10];
-    char preferableDeliveryDate[10];
-    char deliveryDate[10];
-    char pickupAddress[46];
-    char pickupCity[46];
-    int pickupZipCode;
-    char deliveryAddress[46];
-    char deliveryCity[46];
-    int deliveryZipCode;
-    float weight;
-    float volume;
-} FREIGHT;
--- src/Freight.h
+++ src/Freight.h
@@ -5,6 +5,35 @@
 #ifndef FREIGHT_COMPANY_FREIGHT_H
 #define FREIGHT_COMPANY_FREIGHT_H
 
-typedef struct freight FREIGHT;
+typedef struct freight {
+    int id;
+    int clientId;
+    char designation[25];
+    char priority[36];
+    char preferablePickUpDate[10];
+    char pickUpDate[10];
+    char preferableDeliveryDate[10];
+    char deliveryDate[10];
+    char pickupAddress[46];
+    char pickupCity[46];
+    int pickupZipCode;
+    char deliveryAddress[46];
+    char deliveryCity[46];
+    int deliveryZipCode;
+    float weight;
+    float volume;
+} FREIGHT;
+
+FREIGHT freights[100];
+
+void createFreight();
+
+void readFreights();
+
+void updateFreight();
+
+void deleteFreight();
+
+FREIGHT getFreight(int id);
 
 #endif //FREIGHT_COMPANY_FREIGHT_H
