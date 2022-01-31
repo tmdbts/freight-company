a165e752560650838761ccf2589b37353dddee83
Tiago Silva <tbrancosilva@hotmail.com>
2022-01-31 13:38:12 +0000

    ➕ Added Drivers

--- src/Drivers.h
+++ src/Drivers.h
@@ -5,4 +5,19 @@
 #ifndef FREIGHT_COMPANY_DRIVERS_H
 #define FREIGHT_COMPANY_DRIVERS_H
 
+typedef struct driver {
+    int id;
+    char name[46];
+} DRIVER;
+
+DRIVER drivers[100];
+
+void createDriver();
+
+void readDrivers();
+
+void updateDriver();
+
+void deleteDriver();
+
 #endif //FREIGHT_COMPANY_DRIVERS_H
